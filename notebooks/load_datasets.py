c5cce328f512c5bf4c158c393d1aaf42ffe3f4e4
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-04-20 08:15:57 +0200

    readme add missing datasets to table

--- notebooks/load_datasets.py
+++ notebooks/load_datasets.py
@@ -8,7 +8,7 @@ matminer_datasets = get_available_datasets()
 matbench_datasets = [dset for dset in matminer_datasets if dset.startswith("matbench_")]
 
 
-print(f"{matbench_datasets=} (total: {len(matbench_datasets)})")
+print(f"total datasets = {len(matbench_datasets)}\n{matbench_datasets=}")
 # 13 datasets in Matbench v0.1 as of Mar 2021:
 #   dielectric, expt_gap, expt_is_metal, glass,
 #   jdft2d, log_gvrh, log_kvrh, mp_e_form, mp_gap,
